4f303c392aeb5e93030c3609b5ee91e98f0a210d
Sylvan <senguo@Sylvans-MacBook-Pro.local>
2023-03-08 14:29:14 -0800

    simgleton design pattern exaple

---
+++ AnimalDispatchCenter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace SingletonDesignPattern
+{
+	public class AnimalDispatchCenter
+	{
+		private static readonly AnimalDispatchCenter _dispatchCenterInstance = new AnimalDispatchCenter();
+		private List<string> animalsForDispatch = new List<string>();
+        private int nextAnimaldispatcher = 0;
+        private AnimalDispatchCenter()
+		{
+            animalsForDispatch.Add("wiggly");
+            animalsForDispatch.Add("nibbly");
+            animalsForDispatch.Add("furry");
+            animalsForDispatch.Add("slithery");
+            animalsForDispatch.Add("sneaky");
+            animalsForDispatch.Add("wimpy");
+            animalsForDispatch.Add("jumpy");
+        }
+
+        public static AnimalDispatchCenter GetAnimalDispatchCenter()
+        {
+            return _dispatchCenterInstance;
+        }
+
+        public string GetNextAnimalDispatcher()
+        {
+            string nextDispatcher = animalsForDispatch[nextAnimaldispatcher];
+            nextAnimaldispatcher += 1;
+
+            if (nextAnimaldispatcher > animalsForDispatch.Count - 1)
+            {
+                nextAnimaldispatcher = 0;
+            }
+            return nextDispatcher;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SingletonDesignPattern;
+class Program
+{
+    static AnimalDispatchCenter dispatchTeamOne = AnimalDispatchCenter.GetAnimalDispatchCenter();
+    static AnimalDispatchCenter dispatchTeamTwo = AnimalDispatchCenter.GetAnimalDispatchCenter();
+
+
+    static void Main(string[] args)
+    {
+        //AnimalDispatchCenter dispatchCenter = AnimalDispatchCenter.GetAnimalDispatchCenter();
+        for (int i = 0; i < 7; i++)
+        {
+            teamOneGetNextDispatcher();
+            teamTwoGetNextDispatcher();
+        }
+        
+
+    }
+    private static void teamOneGetNextDispatcher()
+    {
+        Console.WriteLine("The next dispatcher is: " + dispatchTeamOne.GetNextAnimalDispatcher());
+    }
+
+    private static void teamTwoGetNextDispatcher()
+    {
+        Console.WriteLine("The next dispatcher is: " + dispatchTeamTwo.GetNextAnimalDispatcher());
+    }
+}
